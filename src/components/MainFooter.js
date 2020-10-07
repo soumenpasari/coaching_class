f8ba0545cdbd3a674ab3dfe2a176957f5670c991
soumenpasari <soumenpasari849@gmail.com>
2020-10-07 18:42:31 +0530

    feat: added npm router package

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -7,7 +7,7 @@ export const MainFooter = () => {
                 <div class="container">
                     <ul>
                     <li class="is-active"><a href='/'>Home</a></li>
-                    <li><a href='/'>Why join us</a></li>
+                    <li><a href='/'>Why to join us</a></li>
                     <li><a href='/'>About us</a></li>
                     <li><a href='/'>Contact us</a></li>
                     </ul>
