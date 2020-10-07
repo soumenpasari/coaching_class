768d650715ad58fcb0a313f800763603d9abb926
soumenpasari <soumenpasari849@gmail.com>
2020-10-07 11:55:33 +0530

    feat: main page created

---
+++ src/components/MainFooter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const MainFooter = () => {
+    return (
+        <div className='hero-foot'>
+            <nav class="tabs is-boxed is-fullwidth">
+                <div class="container">
+                    <ul>
+                    <li class="is-active"><a href='/'>Home</a></li>
+                    <li><a href='/'>Why join us</a></li>
+                    <li><a href='/'>About us</a></li>
+                    <li><a href='/'>Contact us</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
