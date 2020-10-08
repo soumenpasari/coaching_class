8f00c5c810e5728378fbe2c14a7cdf0d74669dcf
soumenpasari <soumenpasari849@gmail.com>
2020-10-08 12:16:04 +0530

    feat: added join us page.

---
+++ src/components/JoinusBody.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export const JoinusBody = () => {
+    return (
+        <div className='section'>
+            <div className='container'>
+                <div className='content'>
+                    <h2 className='subtitle has-text-info'>
+                        Reasons Why  Students should consider concentric the Best in IIT-JEE and beyond.
+                    </h2>
+                    <div className='columns is-multiline'>
+                        <div className='column is-6'>
+                            <div className='box'>
+                                <strong>THE VALUE SYSTEM</strong><br/>
+                                <span>
+                                Concentric value system revolves around truth, transparency & commitment. Whatever we think, we say & whatever we say, we do. We present to you what we actually  are.
+                                </span>
+                            </div>
+                        </div>
+                        <div className='column is-6'>
+                            <div className='box'>
+                                <strong>THE QUALITY</strong><br/>
+                                <span>
+                                We never compromise on quality. Our penchant & pursuit of quality is evident in our every activity, in all our actions, at all times, everywhere.
+                                </span>
+                            </div>
+                        </div>
+                        <div className='column is-6'>
+                            <div className='box'>
+                                <strong>BEST FACULTY</strong><br/>
+                                <span>
+                                We are the only institute that follows a very strong, systematic & objective selection and training process before a teacher becomes a Concentric Faculty. Concentric has a pool of very competent full-time faculty ensuring you get highly effective teachers in all the subjects throughout your studies. 
+                                </span>
+                            </div>
+                        </div>
+                        <div className='column is-6'>
+                            <div className='box'>
+                                <strong>PATTERN PROOF TEACHING </strong><br/>
+                                <span>
+                                Concentric has always been preparing Students in a fundamental way. Concentric  has been teaching in a comprehensive way so that Students do well in IIT-JEE
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
